6f1bf6a97bf27585695f81258558ae26f8ea7395
Muhammet Bas <190201113@uzem.education>
2022-04-02 19:19:18 +0300

    Lesson - 11

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -35,7 +35,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 15)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]   // ATTRIBUTE
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //naming convention
+        // IoC container  --- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            // Dependency chain
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result); // result.Data vs. Ok();
+            }
+            return BadRequest(result); // result.message de yazılablir
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            // Dependency chain
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result); // result.Data vs. Ok();
+            }
+            return BadRequest(result); // result.message de yazılablir
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(Product product)
+        {
+            // Dependency chain
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result); // result.Data vs. Ok();
+            }
+            return BadRequest(result); // result.message de yazılablir
+        }
+
+    }
+}
